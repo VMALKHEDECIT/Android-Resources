01ee0906ec753521b27873ebb1e432a892aab315
VAIBHAVCIT <vmalkhede0@gmail.com>
2022-11-10 06:19:16 +0530

    All Method Programs

---
+++ src/Methods/Display.java
@@ -0,0 +1,15 @@
+package Methods;
+
+//Static Method Program
+
+public class Display
+{
+    public static void main(String[] args)
+    {
+        show();
+    }
+    static void show()
+    {
+        System.out.println("It is an example of static method.");
+    }
+}
